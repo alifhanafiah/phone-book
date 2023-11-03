488c8e906b10def3fc31020961d4d0146ff6665b
alif-hanafiah <muhalifhanafiah@gmail.com>
2023-11-03 23:31:19 +0700

    Adding error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,19 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import ErrorBoundary from './components/ErrorBoundary.tsx';
 
 const client = new ApolloClient({
-  uri: 'https://wpe-hiring.tokopedia.net/',
-
+  uri: 'https://wpe-hiring.tokopedia.net/graphql',
   cache: new InMemoryCache(),
 });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </ApolloProvider>
   </React.StrictMode>
 );
