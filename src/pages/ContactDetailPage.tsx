d94f1a446dfcb0ace022fc15511371bdb99d4242
alif-hanafiah <muhalifhanafiah@gmail.com>
2023-11-04 15:53:22 +0700

    Fetch detail contact data

--- src/pages/ContactDetailPage.tsx
+++ src/pages/ContactDetailPage.tsx
@@ -1,5 +1,34 @@
+import { useQuery } from '@apollo/client';
+import { useParams } from 'react-router-dom';
+import { GET_CONTACT_DETAIL } from '../queries';
+
+type ContactData = {
+  contact_by_pk: {
+    last_name: string;
+    id: number;
+    first_name: string;
+    created_at: string;
+    phones: {
+      number: string;
+    }[];
+  };
+};
+
 const ContactDetailPage = () => {
-  return <div>ContactDetailPage</div>;
+  const { id } = useParams();
+
+  const { loading, error, data } = useQuery<ContactData>(GET_CONTACT_DETAIL, {
+    variables: {
+      id,
+    },
+  });
+
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+  console.log(data?.contact_by_pk);
+
+  return <div>{id}</div>;
 };
 
 export default ContactDetailPage;
