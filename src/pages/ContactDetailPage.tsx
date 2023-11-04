24424cb27c5c3ecebc8dbcceb3ea63a381e887db
alif-hanafiah <muhalifhanafiah@gmail.com>
2023-11-05 00:06:52 +0700

    Finishing the detail page

--- src/pages/ContactDetailPage.tsx
+++ src/pages/ContactDetailPage.tsx
@@ -1,5 +1,9 @@
 import { useQuery } from '@apollo/client';
+import { css } from '@emotion/react';
 import { useParams } from 'react-router-dom';
+import img from '../assets/images/google-contacts.png';
+import { colors } from '../assets/styles/const';
+import Loading from '../components/Loading';
 import { GET_CONTACT_DETAIL } from '../queries';
 
 type ContactData = {
@@ -14,6 +18,40 @@ type ContactData = {
   };
 };
 
+const contactDetailPage = {
+  container: css({
+    display: 'grid',
+    placeItems: 'center',
+    textAlign: 'center',
+    backgroundColor: '#1f2937',
+    borderRadius: '2rem',
+    paddingInline: '.75rem',
+    paddingBlock: '.5rem',
+    marginTop: '3rem',
+  }),
+
+  image: css({
+    width: '5rem',
+    marginTop: '-3rem',
+  }),
+
+  divider: css({
+    width: '4rem',
+    border: `2px solid #a3a3a3`,
+    borderRadius: '1rem',
+    marginBlock: '1rem',
+  }),
+
+  data: css({
+    color: '#9ca3af',
+    marginBlock: '.5rem',
+
+    'p + p': {
+      color: colors.secondary,
+    },
+  }),
+};
+
 const ContactDetailPage = () => {
   const { id } = useParams();
 
@@ -23,12 +61,45 @@ const ContactDetailPage = () => {
     },
   });
 
-  if (loading) return 'Loading...';
+  if (loading) return <Loading />;
   if (error) return `Error! ${error.message}`;
 
-  console.log(data?.contact_by_pk);
+  const contact = data?.contact_by_pk;
 
-  return <div>{id}</div>;
+  // Default value in case created_at is undefined
+  let formattedDate = 'Date not available';
+  if (contact && contact.created_at) {
+    const date = new Date(contact.created_at);
+    formattedDate = date.toLocaleDateString('id-ID', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    });
+  }
+
+  return (
+    <div css={contactDetailPage.container}>
+      <img src={img} css={contactDetailPage.image} />
+
+      <hr css={contactDetailPage.divider} />
+
+      <h2>
+        {contact?.first_name} {contact?.last_name}
+      </h2>
+
+      <div css={contactDetailPage.data}>
+        <p>Created at</p>
+        <p>{formattedDate}</p>
+      </div>
+
+      <div css={contactDetailPage.data}>
+        <p>Phone</p>
+        {contact?.phones.map((phone, index) => {
+          return <p key={index}>{phone.number}</p>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ContactDetailPage;
